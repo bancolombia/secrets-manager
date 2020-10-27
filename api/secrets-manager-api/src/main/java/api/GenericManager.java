1602c56f726b606cabef97af2da06033466288cf
Alejandro Betancur Barrientos <alejobtc@gmail.com>
2020-10-27 11:48:52 -0500

    Feature/reactive refactor (#17)
    * Gradle multiproject creation
    
    * Dependencies added
    
    * API done
    
    * AWS Secrects manager implementation
    
    * Env connector implementation
    
    * File Connector implemenentation
    
    * Unused code removed
    
    * Async secrets manager implementation
    
    * Cache for secrets implemented
    
    * Logs and error management implemented
    
    * test for sync aws connector
    
    * Build gradles updated for publishing
    
    * Build gradles fixed for publishing
    
    * checking workflow sintax
    
    * Publishing test
    
    * Fixed Deploy Config
    
    * Release test
    
    * ignoring tests
    
    * comment sonar task
    
    * delete on push on feature
    
    * dynamic discover gradle projects
    
    * Build.gradles renamed
    
    * api gradle fixes
    
    * defalut user in bintray
    
    * unsused imports and implementation change
    
    * add release condition
    
    Co-authored-by: Daniel Rico <danielrico.posada@gmail.com>

---
+++ api/secrets-manager-api/src/main/java/api/GenericManager.java
@@ -0,0 +1,8 @@
+package api;
+
+import exceptions.SecretException;
+
+public interface GenericManager {
+    String getSecret(String secretName) throws SecretException;
+    <T> T getSecret(String secretName, Class<T> cls) throws SecretException;
+}
---
+++ api/secrets-manager-api/src/main/java/api/GenericManagerAsync.java
@@ -0,0 +1,9 @@
+package api;
+
+import exceptions.SecretException;
+import reactor.core.publisher.Mono;
+
+public interface GenericManagerAsync {
+    Mono<String> getSecret(String secretName) throws SecretException;
+    <T> Mono<T> getSecret(String secretName, Class<T> cls) throws SecretException;
+}
