6e7d118519a9142dc720500c8b31819d11d87d18
davideduma <david24eduma@gmail.com>
2022-12-27 16:18:33 -0500

    Add code_of_conduct

--- api/secrets-manager-api/src/main/java/co/com/bancolombia/secretsmanager/api/GenericManagerAsync.java
+++
@@ -1,9 +0,0 @@
-package co.com.bancolombia.secretsmanager.api;;
-
-import co.com.bancolombia.secretsmanager.api.exceptions.SecretException;
-import reactor.core.publisher.Mono;
-
-public interface GenericManagerAsync {
-    Mono<String> getSecret(String secretName) throws SecretException;
-    <T> Mono<T> getSecret(String secretName, Class<T> cls) throws SecretException;
-}
